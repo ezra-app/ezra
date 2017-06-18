1b8d6b98c31ed9bb5a10092f368e28e89e3d95dd
Humberto Machado <betolinck@gmail.com>
2017-06-18 14:53:42 -0300

    Adicionar arquivos de projeto.

---
+++ Ezra/Ezra/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Ezra.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
