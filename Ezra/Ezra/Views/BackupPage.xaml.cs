d9127861ceeaa3b7164d9b5fe8d918fb28e5f456
Humberto Machado <betolinck@gmail.com>
2017-10-24 08:06:37 -0300

    Esquema de bkp temporário

---
+++ Ezra/Ezra/Views/BackupPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Ezra.Views
+{
+    public partial class BackupPage : ContentPage
+    {
+        public BackupPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
