0faf00f6bb0bb00e0fc88f7b364ebc3f8ec6637c
Humberto Machado <betolinck@gmail.com>
2017-11-12 17:32:41 -0300

    Melhorias na tela de edição: Botões de adicionar e subtrair

--- Ezra/Ezra/Views/ReportEditionPage.xaml.cs
+++ Ezra/Ezra/Views/ReportEditionPage.xaml.cs
@@ -31,9 +31,75 @@ namespace Ezra.Views
             if (string.IsNullOrEmpty(sender.Text) || int.TryParse(sender.Text, out var n))
             {
                 sender.Text = e.NewTextValue;
-            } else
+            }
+            else
             {
                 sender.Text = e.OldTextValue;
+            }
+        }
+
+        private void OnReturnVisitsPlusClicked(object sender, EventArgs e)
+        {
+            PlusButtonHandler(ReturnVisitsEntry);
+        }
+
+        private void OnReturnVisitsMinusClicked(object sender, EventArgs e)
+        {
+            MinusButtonHandler(ReturnVisitsEntry);
+        }
+
+        private void OnPlacementsPlusClicked(object sender, EventArgs e)
+        {
+            PlusButtonHandler(PlacementsEntry);
+        }
+
+        private void OnPlacementsMinusClicked(object sender, EventArgs e)
+        {
+            MinusButtonHandler(PlacementsEntry);
+        }
+
+        private void OnVideosPlusClicked(object sender, EventArgs e)
+        {
+            PlusButtonHandler(VideosEntry);
+        }
+
+        private void OnVideosMinusClicked(object sender, EventArgs e)
+        {
+            MinusButtonHandler(VideosEntry);
+        }
+
+        private void OnStudiesPlusClicked(object sender, EventArgs e)
+        {
+            PlusButtonHandler(StudiesEntry);
+        }
+
+        private void OnStudiesMinusClicked(object sender, EventArgs e)
+        {
+            MinusButtonHandler(StudiesEntry);
+        }
+
+        private void PlusButtonHandler(Entry entry)
+        {
+            var valueText = entry.Text;
+            if (string.IsNullOrEmpty(valueText))
+            {
+                valueText = "0";
+            }
+
+            entry.Text = (Convert.ToInt16(valueText) + 1).ToString();
+        }
+
+        private void MinusButtonHandler(Entry entry)
+        {
+            var valueText = entry.Text;
+            if (string.IsNullOrEmpty(valueText))
+            {
+                valueText = "0";
+            }
+            var valueInt = Convert.ToInt16(valueText);
+            if (valueInt > 0)
+            {
+                entry.Text = (Convert.ToInt16(valueText) - 1).ToString();
             }
         }
     }
